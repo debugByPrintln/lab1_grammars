5fe189a873c158a4fef8ce72ce3e864536e993e8
Nikita Melnikov <melnikov.n.89@mail.ru>
2023-05-14 17:20:20 +0300

    td

--- src/main.py
+++ src/main.py
@@ -23,31 +23,87 @@ follow = {
 }
 
 def S():
-    pass
+    A()
+    B()
+    C()
+    c = getchar()
+    if c != '$':
+        end()
 
 def A():
-    pass
+    c = getchar()
+    if c == 'b':
+        A____()
+    else:
+        end()
 
 def A____():
-    pass
+    c = getchar()
+    if c in first['A_']:
+        A_()
+    elif c is None:
+        return
+    else:
+        end()
 
 def A_():
-    pass
+    C()
+    c = getchar()
+    if c == 'b':
+        A__()
+    else:
+        end()
 
 def A__():
-    pass
+    c = getchar()
+    if c in follow['A_']:
+        return
+    elif c == 'b':
+        A___()
+    elif c is None:
+        return
+    else:
+        end()
+
 
 def A___():
-    pass
+    c = getchar()
+    if c in first['A_']:
+        A_()
+    elif c is None:
+        return
+    else:
+        end()
 
 def C():
-    pass
+    c = getchar()
+    if c == 'b':
+        c = getchar()
+        if c == 'b':
+            C_()
+        else:
+            return
+    else:
+        end()
 
 def C_():
-    pass
+    c = getchar()
+    if c in follow['B']:
+        return
+    elif c == 'a':
+        A()
+    else:
+        end()
+
 
 def B():
-    pass
+    c = getchar()
+    if c == 'b':
+        c = getchar()
+        if c != 'b':
+            end()
+    else:
+        end()
 
 def end():
     print("Слово не подходит под грамматику вида:\nS → ACB\nA → bA’’’’\nA’’’’ → A’ | Ɛ\nA’ → CbA’’\nA’’ → A’ | bA’’’ | Ɛ\nA’’’ → A’ | Ɛ\nC → bbC’  |  c\nC’ → aA | B\nB → bb")
@@ -67,3 +123,5 @@ def start(st):
     str1 = st
     S()
     print("Слово подходит под грамматику вида:\nS → ACB\nA → bA’’’’\nA’’’’ → A’ | Ɛ\nA’ → CbA’’\nA’’ → A’ | bA’’’ | Ɛ\nA’’’ → A’ | Ɛ\nC → bbC’  |  c\nC’ → aA | B\nB → bb")
+
+start("bcbb")
